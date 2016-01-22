86856ead517447bfe89422e880bc6d6f63a79a8d
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-01-22 12:31:24 -1000

    4. JOIN.sql

--- joins.sql
+++ joins.sql
@@ -7,8 +7,16 @@
 -- FROM posts
 -- WHERE user_id=100;
 
---3
-SELECT p.*, u.first_name, u.last_name
-FROM posts p
-INNER JOIN users u ON p.user_id = u.id
-WHERE u.id = 200;
+-- --3
+-- SELECT p.*, u.first_name, u.last_name
+-- FROM posts p
+-- INNER JOIN users u ON p.user_id = u.id
+-- WHERE u.id = 200;
+
+-- --4
+-- SELECT p.*, u.username
+-- FROM posts p
+-- INNER JOIN users u ON p.user_id = u.id
+-- WHERE u.id = 200;
+
+--5
