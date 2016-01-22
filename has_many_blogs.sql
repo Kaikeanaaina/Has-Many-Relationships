4d7452ed4f20556460ff3caeaf759520b424455b
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-01-21 18:55:57 -1000

    fixed some syntax errors

--- has_many_blogs.sql
+++ has_many_blogs.sql
@@ -12,31 +12,31 @@ CREATE DATABASE has_many_blogs OWNER has_many_user;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users(
-"id" DATATYPE serial NOT NULL PRIMARY KEY,
-"username" DATATYPE character varying(90) NOT NULL,
-"first_name" DATATYPE character varying(90) NULL DEFAULT NULL,
-"last_name" DATATYPE character varying(90) NULL DEFAULT NULL,
-"created_at" DATATYPE timestamp WITH time zone NOT NULL DEFAULT now(),
-"updated_at" DATATYPE timestamp WITH time zone NOT NULL DEFAULT now()
+"id" serial NOT NULL PRIMARY KEY,
+"username" character varying(90) NOT NULL,
+"first_name" character varying(90) NULL DEFAULT NULL,
+"last_name" character varying(90) NULL DEFAULT NULL,
+"created_at" timestamp WITH time zone NOT NULL DEFAULT now(),
+"updated_at" timestamp WITH time zone NOT NULL DEFAULT now()
 );
 
 DROP TABLE IF EXISTS posts;
 
 CREATE TABLE posts(
-"id" DATATYPE serial NOT NULL PRIMARY KEY,
-"title" DATATYPE character varying(180) NULL DEFAULT NULL,
-"url" DATATYPE character varying(510) NULL DEFAULT NULL,
-"content" DATATYPE text NULL DEFAULT NULL,
-"created_at" DATATYPE timestamp WITH time zone NOT NULL DEFAULT now(),
-"updated_at" DATATYPE timestamp WITH time zone NOT NULL DEFAULT now()
+"id" serial NOT NULL PRIMARY KEY,
+"title" character varying(180) NULL DEFAULT NULL,
+"url" character varying(510) NULL DEFAULT NULL,
+"content" text NULL DEFAULT NULL,
+"created_at" timestamp WITH time zone NOT NULL DEFAULT now(),
+"updated_at" timestamp WITH time zone NOT NULL DEFAULT now()
 );
 
 DROP TABLE IF EXISTS comments;
 
 CREATE TABLE comments(
-"id" DATATYPE serial NOT NULL PRIMARY KEY,
+"id" serial NOT NULL PRIMARY KEY,
 "book" character varying(510) NULL DEFAULT NULL,
-"created_at" DATATYPE timestamp WITH time zone NOT NULL DEFAULT now(),
-"updated_at" DATATYPE timestamp WITH time zone NOT NULL DEFAULT now(),
+"created_at" timestamp WITH time zone NOT NULL DEFAULT now(),
+"updated_at" timestamp WITH time zone NOT NULL DEFAULT now()
 );
 
