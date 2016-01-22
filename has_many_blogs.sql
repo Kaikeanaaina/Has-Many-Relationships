079eec2239564422631c03d929c691a54c26d9c3
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-01-22 11:36:56 -1000

    user, posts, and comments table inserted from the other file

--- has_many_blogs.sql
+++ has_many_blogs.sql
@@ -1,48 +1,54 @@
-DROP DATABASE IF EXISTS has_many_blogs;
+-- DROP DATABASE IF EXISTS has_many_blogs;
 
-DROP USER IF EXISTs has_many_user;
+-- DROP USER IF EXISTs has_many_user;
 
-CREATE USER has_many_user;
+-- CREATE USER has_many_user;
 
 
-CREATE DATABASE has_many_blogs OWNER has_many_user;
+-- CREATE DATABASE has_many_blogs OWNER has_many_user;
+
 
 \c has_many_blogs;
 
-DROP TABLE IF EXISTS users;
-
-CREATE TABLE users(
-"id" serial NOT NULL PRIMARY KEY,
-"username" character varying(90) NOT NULL,
-"first_name" character varying(90) NULL DEFAULT NULL,
-"last_name" character varying(90) NULL DEFAULT NULL,
-"created_at" timestamp WITH time zone NOT NULL DEFAULT now(),
-"updated_at" timestamp WITH time zone NOT NULL DEFAULT now()
-);
-
-DROP TABLE IF EXISTS posts;
-
-CREATE TABLE posts(
-"id" serial NOT NULL PRIMARY KEY,
-"user_id" integer REFERENCES users(id),
-"title" character varying(180) NULL DEFAULT NULL,
-"url" character varying(510) NULL DEFAULT NULL,
-"content" text NULL DEFAULT NULL,
-"created_at" timestamp WITH time zone NOT NULL DEFAULT now(),
-"updated_at" timestamp WITH time zone NOT NULL DEFAULT now()
-);
 
 DROP TABLE IF EXISTS comments;
+DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS users;
+
+
+CREATE TABLE users(
+id serial NOT NULL PRIMARY KEY,
+username character varying(90) NOT NULL,
+first_name character varying(90) DEFAULT NULL,
+last_name character varying(90) DEFAULT NULL,
+created_at timestamp WITH time zone NOT NULL DEFAULT now(),
+updated_at timestamp WITH time zone NOT NULL DEFAULT now()
+);
+
+
+CREATE TABLE posts(
+id serial NOT NULL PRIMARY KEY,
+title character varying(180) DEFAULT NULL,
+url character varying(510) DEFAULT NULL,
+content text DEFAULT NULL,
+created_at timestamp WITH time zone NOT NULL DEFAULT now(),
+updated_at timestamp WITH time zone NOT NULL DEFAULT now(),
+user_id integer REFERENCES users(id)
+);
+
 
 CREATE TABLE comments(
-"id" serial NOT NULL PRIMARY KEY,
-"user_id" integer REFERENCES users(id),
-"post_id" integer REFERENCES posts(id),
-"book" character varying(510) NULL DEFAULT NULL,
-"created_at" timestamp WITH time zone NOT NULL DEFAULT now(),
-"updated_at" timestamp WITH time zone NOT NULL DEFAULT now()
+id serial NOT NULL PRIMARY KEY,
+book character varying(510) DEFAULT NULL,
+created_at timestamp WITH time zone NOT NULL DEFAULT now(),
+updated_at timestamp WITH time zone NOT NULL DEFAULT now(),
+user_id integer REFERENCES users(id),
+post_id integer REFERENCES posts(id)
 );
 
 --constraints
+
+
+
 
 
