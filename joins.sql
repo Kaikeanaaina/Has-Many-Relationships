7dcc6955b3b7bfeb41edebd410dbb8cf92b2fcfc
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-01-22 14:47:14 -1000

    6 join.sql

--- joins.sql
+++ joins.sql
@@ -19,4 +19,15 @@
 -- INNER JOIN users u ON p.user_id = u.id
 -- WHERE u.id = 200;
 
---5
+-- --5
+-- SELECT u.username, p.user_id
+-- FROM users u
+-- INNER JOIN posts p ON u.id = p.user_id
+-- WHERE p.created_at > '2015-01-01';
+
+-- --6
+-- SELECT p.title, u.username
+-- FROM posts p
+-- INNER JOIN users u ON p.user_id=u.id
+-- WHERE u.created_at > '2015-01-01';
+
